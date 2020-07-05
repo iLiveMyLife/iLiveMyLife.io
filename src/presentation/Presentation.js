daa5e53ec1044010093a27c15431c23e95e3c9df
Ilya Sorokin <silarr@users.noreply.github.com>
2020-07-05 15:17:12 -0700

    changing presentation test with link to Login from it

--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Empty, Button } from 'antd';
 import './Presentation.css';
+import { APP_URL } from '../constants';
 
 class Presentation extends Component {
     render() {
@@ -12,11 +13,13 @@ class Presentation extends Component {
                 }}
                 description={
                     <span>
-                        Congratulations! You are one step closer to create your new Life (the other way)
+                        Congratulations! You've finally found your future self.
+                        <br />
+                        You are one step closer to create your new Life (the other way)
                     </span>
                 }
             >
-                <Button type="primary">Create Now</Button>
+                <Button type="primary"><a href={APP_URL + "/login"}>Create</a></Button>
             </Empty>
         );
     }
