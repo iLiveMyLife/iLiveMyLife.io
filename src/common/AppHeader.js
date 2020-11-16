142b0cd09a0e40f46aa68c4e24d4a449436ec2e0
Ilya Sorokin <silarr@users.noreply.github.com>
2020-11-15 17:38:31 -0800

    Changing presentation page's slogans

--- src/common/AppHeader.js
+++ src/common/AppHeader.js
@@ -4,8 +4,8 @@ import {
     withRouter
 } from 'react-router-dom';
 import './AppHeader.css';
-import { APP_URL } from '../constants';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Dropdown } from 'antd';
+import { HomeOutlined, UserOutlined, DownOutlined } from '@ant-design/icons';
 const Header = Layout.Header;
 
 class AppHeader extends Component {
@@ -15,26 +15,49 @@ class AppHeader extends Component {
     }
 
     handleMenuClick({ key }) {
-      if(key === "demo") {
-       // TODO
+      if(key === "logout") {
+        this.props.onLogout();
       }
     }
 
     render() {
-        let menuItems = [
-            <Menu.Item key="/login">
-              <a href={APP_URL + "/login"}>Login</a>
+        let menuItems;
+        if(this.props.currentUser) {
+          menuItems = [
+            <Menu.Item key="/">
+              <Link to="/">
+                  <HomeOutlined className="nav-icon"/>
+              </Link>
             </Menu.Item>,
-            <Menu.Item key="/signup">
-              <a href={APP_URL + "/signup"}>Signup</a>
+              /*
+            <Menu.Item key="/poll/new">
+                <Link to="/poll/new">
+                    <img src={pollIcon} alt="poll" className="poll-icon" />
+                </Link>
+            </Menu.Item>,
+            */
+            <Menu.Item key="/profile" className="profile-menu">
+                <ProfileDropdownMenu
+                  currentUser={this.props.currentUser}
+                  handleMenuClick={this.handleMenuClick}/>
             </Menu.Item>
           ];
+        } else {
+          menuItems = [
+            <Menu.Item key="/login">
+              <Link to="/login">Login</Link>
+            </Menu.Item>,
+            <Menu.Item key="/signup">
+              <Link to="/signup">Signup</Link>
+            </Menu.Item>
+          ];
+        }
 
         return (
             <Header className="app-header">
             <div className="container">
               <div className="app-title" >
-                <Link to="/">iLiveMyLife App</Link>
+                <Link to="/">iLiveMyLife, and so are you!</Link>
               </div>
               <Menu
                 className="app-menu"
@@ -47,6 +70,42 @@ class AppHeader extends Component {
           </Header>
         );
     }
+}
+
+function ProfileDropdownMenu(props) {
+  const dropdownMenu = (
+    <Menu onClick={props.handleMenuClick} className="profile-dropdown-menu">
+      <Menu.Item key="user-info" className="dropdown-item" disabled>
+        <div className="user-full-name-info">
+          {props.currentUser.displayName ? props.currentUser.displayName : null}
+        </div>
+        <div className="username-info">
+          @{props.currentUser.username}
+        </div>
+      </Menu.Item>
+      <Menu.Divider />
+        {/*
+      <Menu.Item key="profile" className="dropdown-item">
+        <Link to={`/users/${props.currentUser.username}`}>Profile</Link>
+      </Menu.Item>
+      */}
+      <Menu.Item key="logout" className="dropdown-item">
+           Logout
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Dropdown
+      overlay={dropdownMenu}
+      trigger={['click']}
+      getPopupContainer = { () => document.getElementsByClassName('profile-menu')[0]}>
+      <span className="ant-dropdown-link">
+         <UserOutlined className="nav-icon" style={{marginRight: 0}} />
+         <DownOutlined />
+      </span>
+    </Dropdown>
+  );
 }
 
 export default withRouter(AppHeader);
--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import {Empty, Button } from 'antd';
 import './Presentation.css';
-import { APP_URL } from '../constants';
 
-export default class Presentation extends Component {
+class Presentation extends Component {
     render() {
         return (
             <Empty
@@ -13,14 +12,16 @@ export default class Presentation extends Component {
                 }}
                 description={
                     <span>
-                        Congrats! You've finally found your future self!
-                        <br />
-                        You are one step closer to create your new Life
+                        <h3>Congratulations!</h3>
+                        <h2>You are one step closer to create your new Life.</h2>
+                        <h1>Organically in synergy with computers.</h1>
                     </span>
                 }
             >
-                <Button type="primary"><a href={APP_URL + "/signup"}>Create</a></Button>
+                <Button type="primary">Create Now</Button>
             </Empty>
         );
     }
 }
+
+export default Presentation;
