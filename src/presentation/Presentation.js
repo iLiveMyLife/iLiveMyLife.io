5db17aec25022d63347e79a2b95b25a7b0b956dd
Ilya Sorokin <silarr@users.noreply.github.com>
2020-11-15 20:37:18 -0800

    Removing empty component from Presentation page

--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
-import {Empty, Button } from 'antd';
+import {Typography, Button } from 'antd';
 import './Presentation.css';
 
 class Presentation extends Component {
-
     render() {
-        console.log(this.props.currentUser)
         return (
-            <Empty
-                image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
-                imageStyle={{
-                    height: 60,
-                }}
-                description={
-                    <span>
-                        <h3>Congratulations!</h3>
-                        <h2>You are one step closer to create your new Life.</h2>
-                        <h1>Organically in synergy with computers.</h1>
-                    </span>
-                }
-            >
+            <div className={"presentaion-center"}>
+                <h4>Hey, Congratulations!</h4>
+                <h3>You are one step closer to create your new Life.</h3>
+                <h1>Organically in synergy with computers.</h1>
                 <Button type="primary">
                     <a href="https://app.ilivemylife.io/signup">Create Now</a>
                 </Button>
-            </Empty>
+            </div>
         );
     }
 }
