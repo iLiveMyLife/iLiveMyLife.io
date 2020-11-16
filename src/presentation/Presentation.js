0eb942a2f6db4b950f58b7175260ca10ed611171
Ilya Sorokin <silarr@users.noreply.github.com>
2020-11-15 20:11:22 -0800

    testing current user in subdomain

--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -3,7 +3,9 @@ import {Empty, Button } from 'antd';
 import './Presentation.css';
 
 class Presentation extends Component {
+
     render() {
+        console.log(this.props.currentUser)
         return (
             <Empty
                 image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
