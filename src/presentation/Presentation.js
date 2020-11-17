34ce27c4ca724f100385e8109b0366e715c4ad95
ruslanvasylev <edgeArchitex@gmail.com>
2020-11-16 21:55:43 -0800

    rephrasing

--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -6,8 +6,8 @@ class Presentation extends Component {
     render() {
         return (
             <div className={"presentaion-center"}>
-                <h1>Congratulations,</h1>
-                <h2>You are one step closer to merging your Life with the Hive-mind!</h2>
+                <h2>Congratulations,</h2>
+                <h3>You are a step closer to merging your Life with the Graph-mind!</h3>
                 <Button type="primary">
                     <a href="https://app.ilivemylife.io/signup">Create Now</a>
                 </Button>
