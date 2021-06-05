c86c896bfcb303d142c85f5ee4574a56bc9fead6
Felix-Asante <quamzgraphix@gmail.com>
2021-06-05 20:13:54 +0100

    responsive navbar

--- src/common/AppHeader.js
+++ src/common/AppHeader.js
@@ -1,114 +1,138 @@
-import React, { Component } from 'react';
-import {
-    Link,
-    withRouter
-} from 'react-router-dom';
-import './AppHeader.css';
-import { Layout, Menu, Dropdown } from 'antd';
-import { HomeOutlined, UserOutlined, DownOutlined } from '@ant-design/icons';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import "./AppHeader.css";
+import { Layout, Menu, Dropdown } from "antd";
+import { HomeOutlined, UserOutlined, DownOutlined } from "@ant-design/icons";
+import { useState } from "react";
+
 const Header = Layout.Header;
 
 class AppHeader extends Component {
-    constructor(props) {
-        super(props);
-        this.handleMenuClick = this.handleMenuClick.bind(this);
-    }
+	constructor(props) {
+		super(props);
 
-    handleMenuClick({ key }) {
-      if(key === "logout") {
-        this.props.onLogout();
-      }
-    }
+		this.state = {
+			menuShow: "hide",
+		};
 
-    render() {
-        let menuItems;
-        if(this.props.currentUser) {
-          menuItems = [
-            <Menu.Item key="/">
-              <Link to="/">
-                  <HomeOutlined className="nav-icon"/>
-              </Link>
-            </Menu.Item>,
-              /*
+		this.handleMenuClick = this.handleMenuClick.bind(this);
+		this.updateMenuStatus = this.updateMenuStatus.bind(this);
+	}
+
+	updateMenuStatus = () => {
+		if (this.state.menuShow == "hide") {
+			this.setState({ menuShow: "show" });
+		} else {
+			this.setState({ menuShow: "hide" });
+		}
+	};
+
+	handleMenuClick({ key }) {
+		if (key === "logout") {
+			this.props.onLogout();
+		}
+	}
+	render() {
+		let menuItems;
+
+		if (this.props.currentUser) {
+			menuItems = [
+				<Menu.Item key="/">
+					<Link to="/">
+						<HomeOutlined className="nav-icon" />
+					</Link>
+				</Menu.Item>,
+				/*
             <Menu.Item key="/poll/new">
                 <Link to="/poll/new">
                     <img src={pollIcon} alt="poll" className="poll-icon" />
                 </Link>
             </Menu.Item>,
             */
-            <Menu.Item key="/profile" className="profile-menu">
-                <ProfileDropdownMenu
-                  currentUser={this.props.currentUser}
-                  handleMenuClick={this.handleMenuClick}/>
-            </Menu.Item>
-          ];
-        } else {
-          menuItems = [
-            <Menu.Item key="/graph">
-              <a href="https://app.ilivemylife.io/graph/000001736056c2cb-2655b2cf545d0001">Project's Graph</a>
-            </Menu.Item>,
-            <Menu.Item key="/login">
-              <a href="https://app.ilivemylife.io/login">Login</a>
-            </Menu.Item>,
-            <Menu.Item key="/signup">
-                <a href="https://app.ilivemylife.io/signup">Signup</a>
-            </Menu.Item>
-          ];
-        }
+				<Menu.Item key="/profile" className="profile-menu">
+					<ProfileDropdownMenu
+						currentUser={this.props.currentUser}
+						handleMenuClick={this.handleMenuClick}
+					/>
+				</Menu.Item>,
+			];
+		} else {
+			menuItems = [
+				<Menu.Item key="/graph">
+					<a href="https://app.ilivemylife.io/graph/000001736056c2cb-2655b2cf545d0001">
+						Project's Graph
+					</a>
+				</Menu.Item>,
+				<Menu.Item key="/login">
+					<a href="https://app.ilivemylife.io/login">Login</a>
+				</Menu.Item>,
+				<Menu.Item key="/signup">
+					<a href="https://app.ilivemylife.io/signup">Signup</a>
+				</Menu.Item>,
+			];
+		}
 
-        return (
-            <Header className="app-header">
-            <div className="container">
-              <div className="app-title" >
-                <Link to="/">iLiveMyLife ...and so do you</Link>
-              </div>
-              <Menu
-                className="app-menu"
-                mode="horizontal"
-                selectedKeys={[this.props.location.pathname]}
-                style={{ lineHeight: '64px' }} >
-                  {menuItems}
-              </Menu>
-            </div>
-          </Header>
-        );
-    }
+		return (
+			<Header className="app-header">
+				<div className="container">
+					<div className="app-title">
+						<Link to="/">iLiveMyLife ...and so do you</Link>
+					</div>
+					<Menu
+						className="app-menu"
+						id={this.state.menuShow}
+						mode="horizontal"
+						selectedKeys={[this.props.location.pathname]}
+						style={{ lineHeight: "64px" }}
+					>
+						{menuItems}
+					</Menu>
+					<div className="hamburger-menu" onClick={this.updateMenuStatus}>
+						<span className="line1"></span>
+						<span className="line2"></span>
+						<span className="line2"></span>
+					</div>
+				</div>
+			</Header>
+		);
+	}
 }
 
 function ProfileDropdownMenu(props) {
-  const dropdownMenu = (
-    <Menu onClick={props.handleMenuClick} className="profile-dropdown-menu">
-      <Menu.Item key="user-info" className="dropdown-item" disabled>
-        <div className="user-full-name-info">
-          {props.currentUser.displayName ? props.currentUser.displayName : null}
-        </div>
-        <div className="username-info">
-          @{props.currentUser.username}
-        </div>
-      </Menu.Item>
-      <Menu.Divider />
-        {/*
+	const dropdownMenu = (
+		<Menu onClick={props.handleMenuClick} className="profile-dropdown-menu">
+			<Menu.Item key="user-info" className="dropdown-item" disabled>
+				<div className="user-full-name-info">
+					{props.currentUser.displayName ? props.currentUser.displayName : null}
+				</div>
+				<div className="username-info">@{props.currentUser.username}</div>
+			</Menu.Item>
+			<Menu.Divider />
+			{/*
       <Menu.Item key="profile" className="dropdown-item">
         <Link to={`/users/${props.currentUser.username}`}>Profile</Link>
       </Menu.Item>
       */}
-      <Menu.Item key="logout" className="dropdown-item">
-           Logout
-      </Menu.Item>
-    </Menu>
-  );
+			<Menu.Item key="logout" className="dropdown-item">
+				Logout
+			</Menu.Item>
+		</Menu>
+	);
 
-  return (
-    <Dropdown
-      overlay={dropdownMenu}
-      trigger={['click']}
-      getPopupContainer = { () => document.getElementsByClassName('profile-menu')[0]}>
-      <span className="ant-dropdown-link">
-         <UserOutlined className="nav-icon" style={{marginRight: 0}} />
-         <DownOutlined />
-      </span>
-    </Dropdown>
-  );
+	return (
+		<Dropdown
+			overlay={dropdownMenu}
+			trigger={["click"]}
+			getPopupContainer={() =>
+				document.getElementsByClassName("profile-menu")[0]
+			}
+		>
+			<span className="ant-dropdown-link">
+				<UserOutlined className="nav-icon" style={{ marginRight: 0 }} />
+				<DownOutlined />
+			</span>
+		</Dropdown>
+	);
 }
 
 export default withRouter(AppHeader);
