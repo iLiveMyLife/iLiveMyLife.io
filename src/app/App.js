bcd68909d4b9a78c362ed99012f8f7dd80ae2cad
Ilya Sorokin <silarr@users.noreply.github.com>
2021-06-14 23:57:08 -0700

    fixing styling mostly on carousel

--- src/app/App.js
+++ src/app/App.js
@@ -67,85 +67,75 @@ class App extends Component {
     if(this.state.isLoading) {
       return <LoadingIndicator />
     }
+
     return (
         <Layout className="app-container">
-          <AppHeader
+            <AppHeader
               isAuthenticated={this.state.isAuthenticated}
               currentUser={this.state.currentUser}
               onLogout={this.handleLogout} />
 
-          <Content className="app-content">
-          <Carousel afterChange={onChange}>  
-            <div className="container">
-              <Switch>
-                <Route exact path="/" render={(props) => <Presentation currentUser={this.state.currentUser} />}>
-                </Route>
-                <Route path="/demo"
-                  render={(props) => <Presentation currentUser={this.state.currentUser} />}>
-                </Route>
-                <Route component={NotFound}></Route>
-              </Switch>
-            </div>
-            <div className="content-style">
-      <h3>Yes, you!! Become an symbiose of machine learning and your creativity
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>The tool to live on the beach
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Multi-dimensional todo list? Here you go!
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Organize your life in a new type of thought gathering process
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Manage teams via individualism, freedom, not corporate structure and hierarchy
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>We are built on top of hierarchy, the free one!
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Open sourcing thoughts
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Build things together, brings what you have already built as a resource
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Idea mining
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Distributed project management
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Prognostic problem solving
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>Crypto communism
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>It is like a Mendeleev table but better, since you can create a Mendeleev table yourself
-</h3>
-    </div>
-    <div className="content-style">
-      <h3>We are a global suppliers of webhook links - supplier publishes an event and everyone else listens (including machines who are themselves are data structures)
-</h3>
-    </div>
-            </Carousel>
-          </Content>
+            <Content className="app-content">
+                <Carousel afterChange={onChange}>
+                    <div className="container">
+                      <Switch>
+                        <Route exact path="/" render={(props) => <Presentation currentUser={this.state.currentUser} />}>
+                        </Route>
+                        <Route path="/demo"
+                          render={(props) => <Presentation currentUser={this.state.currentUser} />}>
+                        </Route>
+                        <Route component={NotFound}></Route>
+                      </Switch>
+                    </div>
+                    <div className="content-style">
+                        <h3>Is it a dead cat or alife? Let's just handshake that it is a Cat?!!</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Yes, you!! Become a symbiose of machine learning and your creativity</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>The tool to live on the beach</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Multi-dimensional todo list? Here you go!</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Organize your life in a new type of thought gathering process</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Manage teams via individualism, freedom, not corporate structure and hierarchy</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>We are built on top of hierarchy, the free one!</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Open sourcing thoughts</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Build things together, brings what you have already built as a resource</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Idea mining</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Distributed project management</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Prognostic problem solving</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>Crypto communism</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>It is like a Mendeleev table but better, since you can create a Mendeleev table yourself</h3>
+                    </div>
+                    <div className="content-style">
+                      <h3>We are a global suppliers of webhook links - supplier publishes an event and everyone else listens (including machines who are themselves are data structures)</h3>
+                    </div>
+                </Carousel>
+            </Content>
           <Footer className="footer-style">
-              <AppFooter />
+            <AppFooter />
           </Footer>
         </Layout>
     );
