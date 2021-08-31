aacec7d3ec640ee20c6d29ca62c66201d221c11d
Ilya Sorokin <silarr@users.noreply.github.com>
2021-08-30 19:44:02 -0700

    updating main slide

--- src/common/AppFooter.js
+++ src/common/AppFooter.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './AppFooter.css';
 import { MailOutlined } from '@ant-design/icons';
 
-const AppFooter =()=> {
+const AppFooter = () => {
     return (
-        <div className="container ">
+        <div className="container app-footer">
                 <ul className="socials">
                     <li><a href="/">About</a></li>
                     <li><a href={"mailto: info@ilivemylife.io" }><MailOutlined /> Send email</a></li>
