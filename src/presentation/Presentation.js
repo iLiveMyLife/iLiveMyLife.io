0cf680738fa56ab9819405482e248668b18a94a1
Ilya Sorokin <silarr@users.noreply.github.com>
2024-04-10 23:49:59 -0700

    feat: testing carousel

--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -5,9 +5,10 @@ import slideData from './data.json';
 import {LeftOutlined, RightOutlined} from "@ant-design/icons";
 
 class Presentation extends Component {
-  state={
-    data:slideData
-  }
+    state= {
+        data: slideData
+    }
+
     constructor(props) {
         super(props);
         this.next = this.next.bind(this);
@@ -15,39 +16,49 @@ class Presentation extends Component {
         this.carousel = React.createRef();
 
     }
-        next() {
+    next() {
         this.carousel.next();
     }
+
     previous() {
         this.carousel.prev();
     }
+
     render() {
         return (
-                <Row justify="center">
-                    <Col span={2}  className={'control-buttons'}>
-                        <LeftOutlined onClick={this.previous} />
-                    </Col>
-                    <Col span={20}>
+            <Row justify="center">
+                <Col span={2} className={'control-buttons'}>
+                    <LeftOutlined onClick={this.previous} />
+                </Col>
+                <Col span={20}>
+                    <Carousel vertical={true}
+                              autoplay
+                              pauseOnHover={true}
+                              pauseOnDotsHover={true}
+                              dotPosition={"bottom"}
+                              arrows={true}
+                              autoplaySpeed={3000}
+                              accessibility={true}
 
-                    <Carousel vertical={true} autoplay dotPosition={"bottom"} ref={node => (this.carousel = node)} >
+                              ref={node => (this.carousel = node)} >
                         {
                             this.state.data.map((item,index) => (
                                 <div className={"carousel-style"} key={index}>
-                                        <h3>{item.title}</h3>
-                                        <h4 className={'description'}>{item.description}</h4>
-                                        {item.image
-                                            ? item.url
-                                                ? <a href={item.url} title={item.title}><img className="slogan-pic" src={item.image} alt={item.description}/></a>
-                                                : <img  className="slogan-pic" src={item.image} alt={item.description}/>
-                                            : null}
+                                    <h3>{item.title}</h3>
+                                    <h4 className={'description'}>{item.description}</h4>
+                                    {item.image
+                                        ? item.url
+                                            ? <a href={item.url} title={item.title}><img className="slogan-pic" src={item.image} alt={item.description}/></a>
+                                            : <img className="slogan-pic" src={item.image} alt={item.description}/>
+                                        : null}
                                 </div>
                             ))}
                     </Carousel>
-                    </Col>
-                    <Col span={2} className={'control-buttons'} >
+                </Col>
+                <Col span={2} className={'control-buttons'} >
                     <RightOutlined onClick={this.next} />
-                    </Col>
-                </Row>
+                </Col>
+            </Row>
         );
     }
 }
