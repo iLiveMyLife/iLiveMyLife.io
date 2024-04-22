9e5da2a516ecac147af14722032fe5f72c0b3146
Ilya Sorokin <silarr@users.noreply.github.com>
2024-04-21 21:34:01 -0700

    feat: adding description to slides for carousel and also adding google metadata

--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 import {Carousel, Col, Row} from 'antd';
 import './Presentation.css';
 import slideData from './data.json';
@@ -24,6 +25,18 @@ class Presentation extends Component {
         this.carousel.prev();
     }
 
+    renderStructuredData(item) {
+        // Type can be like 'Thing' or SoftwareApplication, Concept, CreativeWork, etc.
+        return {
+            "@context": "http://schema.org",
+            "@type": item.type,
+            "name": item.title,
+            "description": item.description,
+            "image": item.image,
+            "url": item.url // optional: include only if available
+        };
+    }
+
     render() {
         return (
             <Row justify="center">
@@ -31,7 +44,7 @@ class Presentation extends Component {
                     <LeftOutlined onClick={this.previous} />
                 </Col>
                 <Col span={20}>
-                    <Carousel vertical={true}
+                    <Carousel vertical={false}
                               autoplay
                               pauseOnHover={true}
                               pauseOnDotsHover={true}
@@ -39,20 +52,27 @@ class Presentation extends Component {
                               arrows={true}
                               autoplaySpeed={3000}
                               accessibility={true}
-
                               ref={node => (this.carousel = node)} >
                         {
-                            this.state.data.map((item,index) => (
-                                <div className={"carousel-style"} key={index}>
-                                    <h3>{item.title}</h3>
-                                    <h4 className={'description'}>{item.description}</h4>
-                                    {item.image
-                                        ? item.url
-                                            ? <a href={item.url} title={item.title}><img className="slogan-pic" src={item.image} alt={item.description}/></a>
-                                            : <img className="slogan-pic" src={item.image} alt={item.description}/>
-                                        : null}
-                                </div>
-                            ))}
+                            this.state.data.map((item, index) => (
+                                <React.Fragment key={index}>
+                                    {/* Render structured data for each slide */}
+                                    <script type="application/ld+json">
+                                        {JSON.stringify(this.renderStructuredData(item))}
+                                    </script>
+                                    <div className={"carousel-style"}>
+                                        <h3>{item.title}</h3>
+                                        <ReactMarkdown className={'slogan-description'} children={item.description} />
+                                        {item.image &&
+                                            (item.url
+                                                    ? <a href={item.url} title={item.title}><img className="slogan-pic" src={item.image} alt={item.description}/></a>
+                                                    : <img className="slogan-pic" src={item.image} alt={item.description}/>
+                                            )
+                                        }
+                                    </div>
+                                </React.Fragment>
+                            ))
+                        }
                     </Carousel>
                 </Col>
                 <Col span={2} className={'control-buttons'} >
