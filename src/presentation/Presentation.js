918981dab4a1973d2208a737ba6680e7579bea7a
Ilya Sorokin <silarr@users.noreply.github.com>
2024-11-20 04:04:34 -0800

    feat: refactoring to functional component

--- src/presentation/Presentation.js
+++ src/presentation/Presentation.js
@@ -1,53 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component, useEffect, useRef, useState} from 'react';
 import ReactMarkdown from 'react-markdown';
 import {Carousel, Col, Row} from 'antd';
 import './Presentation.css';
 import slideData from './data.json';
 import {LeftOutlined, RightOutlined} from "@ant-design/icons";
 
-class Presentation extends Component {
-    state= {
-        data: slideData,
-        columnHeight: 'auto'
-    }
+const Presentation = () => {
+    const [data, setData] = useState(slideData);
+    const [columnHeight, setColumnHeight] = useState('auto');
+    const carouselRef = useRef(null);
 
-    constructor(props) {
-        super(props);
-        this.next = this.next.bind(this);
-        this.previous = this.previous.bind(this);
-        this.carousel = React.createRef();
-
-    }
-
-    componentDidMount() {
-        this.updateColumnHeight();
-        window.addEventListener('resize', this.updateColumnHeight);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateColumnHeight);
-    }
-
-    updateColumnHeight = () => {
+    const updateColumnHeight = () => {
         // Check if the carousel exists and is rendered
-        if (this.carousel && this.carousel.current) {
-            const currentSlide = this.carousel.current.innerSlider.list.querySelector('.slick-active');
+        if (carouselRef && carouselRef.current) {
+            const currentSlide = carouselRef.current.innerSlider.list.querySelector('.slick-active');
             if (currentSlide) {
                 const height = currentSlide.offsetHeight;
-                this.setState({ columnHeight: `${height}px` });
+                setColumnHeight(`${height}px`);
             }
         }
     };
 
-    next() {
-        this.carousel.next();
-    }
+    useEffect(() => {
+        updateColumnHeight();
+        window.addEventListener('resize', updateColumnHeight);
+        return () => window.removeEventListener('resize', updateColumnHeight);
+    }, []);
 
-    previous() {
-        this.carousel.prev();
-    }
+    const next = () => carouselRef.current.next();
+    const previous = () => carouselRef.current.prev();
 
-    renderStructuredData(item) {
+    const renderStructuredData = (item) => {
         // Type can be like 'Thing' or SoftwareApplication, Concept, CreativeWork, etc.
         return {
             "@context": "http://schema.org",
@@ -59,51 +42,47 @@ class Presentation extends Component {
         };
     }
 
-    render() {
-        const { columnHeight } = this.state;
-
-        return (
-            <Row justify="center">
-                <Col span={2} className={'control-column'} onClick={this.previous} style={{height: columnHeight}}>
-                    <LeftOutlined />
-                </Col>
-                <Col span={20}>
-                    <Carousel vertical={false}
-                              autoplay
-                              pauseOnHover={true}
-                              pauseOnDotsHover={true}
-                              dotPosition={"bottom"}
-                              arrows={true}
-                              autoplaySpeed={3000}
-                              accessibility={true}
-                              ref={node => (this.carousel = node)} >
-                        {
-                            this.state.data.map((item, index) => (
-                                <React.Fragment key={index}>
-                                    {/* Render structured data for each slide */}
-                                    <script type="application/ld+json">
-                                        {JSON.stringify(this.renderStructuredData(item))}
-                                    </script>
-                                    <div className={"carousel-style"}>
-                                        <h3>{item.title}</h3>
-                                        <ReactMarkdown className={'slogan-description'} children={item.description} />
-                                        {item.image &&
-                                            (item.url
-                                                    ? <a href={item.url} title={item.title}><img className="slogan-pic" src={item.image} alt={item.description}/></a>
-                                                    : <img className="slogan-pic" src={item.image} alt={item.description}/>
-                                            )
-                                        }
-                                    </div>
-                                </React.Fragment>
-                            ))
-                        }
-                    </Carousel>
-                </Col>
-                <Col span={2} className={"control-column"} onClick={this.next} style={{height: columnHeight}}>
-                    <RightOutlined />
-                </Col>
-            </Row>
-        );
-    }
+    return (
+        <Row justify="center">
+            <Col span={2} className={'control-column'} onClick={previous} style={{height: columnHeight}}>
+                <LeftOutlined />
+            </Col>
+            <Col span={20}>
+                <Carousel vertical={false}
+                          autoplay
+                          pauseOnHover={true}
+                          pauseOnDotsHover={true}
+                          dotPosition={"bottom"}
+                          arrows={true}
+                          autoplaySpeed={3000}
+                          accessibility={true}
+                          ref={carouselRef} >
+                    {
+                        data.map((item, index) => (
+                            <React.Fragment key={index}>
+                                {/* Render structured data for each slide */}
+                                <script type="application/ld+json">
+                                    {JSON.stringify(renderStructuredData(item))}
+                                </script>
+                                <div className={"carousel-style"}>
+                                    <h3>{item.title}</h3>
+                                    <ReactMarkdown className={'slogan-description'} children={item.description} />
+                                    {item.image &&
+                                        (item.url
+                                                ? <a href={item.url} title={item.title}><img className="slogan-pic" src={item.image} alt={item.description}/></a>
+                                                : <img className="slogan-pic" src={item.image} alt={item.description}/>
+                                        )
+                                    }
+                                </div>
+                            </React.Fragment>
+                        ))
+                    }
+                </Carousel>
+            </Col>
+            <Col span={2} className={"control-column"} onClick={next} style={{height: columnHeight}}>
+                <RightOutlined />
+            </Col>
+        </Row>
+    );
 }
 export default Presentation;
